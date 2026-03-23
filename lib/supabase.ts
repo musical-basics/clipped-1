66ccac8efa34eb6b253d746293d615c427fd4dc0
test2 <test2@Lionels-MacBook-Pro-NEW.local>
2026-03-23 14:44:19 -0400

    Fix: add dev script, switch to service role key

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -2,9 +2,10 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL ?? "";
-const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY ?? "";
+const supabaseServiceRoleKey =
+  process.env.EXPO_PUBLIC_SUPABASE_SERVICE_ROLE_KEY ?? "";
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient(supabaseUrl, supabaseServiceRoleKey, {
   auth: {
     storage: AsyncStorage,
     autoRefreshToken: true,
